ff84d7d2c5d834b39e6e279d2cd84eb463c21a15
Xavi Mur <xavier.mur@gmail.com>
2025-10-21 22:41:52 +0200

    feat: initialize project with Expo and TypeScript configuration
    - Added package.json with dependencies for Expo, React, and React Native.
    - Included scripts for starting the app on different platforms (Android, iOS, Web).
    - Created tsconfig.json for TypeScript configuration with strict settings and module resolution.

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>🚀 Hola desde mi app minimal Expo + TypeScript!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 18,
+  },
+});
